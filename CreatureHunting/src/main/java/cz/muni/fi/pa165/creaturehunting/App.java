71336682dcaa58089cab9fa5b409883b5cc39760
Ladushka <kralickova.lebka@gmail.com>
2014-10-08 16:26:38 +0200

    První commit :)

---
+++ CreatureHunting/src/main/java/cz/muni/fi/pa165/creaturehunting/App.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.creaturehunting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
