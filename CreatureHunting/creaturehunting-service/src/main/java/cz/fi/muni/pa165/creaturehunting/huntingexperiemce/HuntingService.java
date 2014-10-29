b214357ececa5a733f6b4ced2540ce8ef866aa41
Flaj <ondrej.flajsman@gmail.com>
2014-10-29 21:04:25 +0100

    Prvotni predstava toho, co mame delat.

---
+++ CreatureHunting/creaturehunting-service/src/main/java/cz/fi/muni/pa165/creaturehunting/huntingexperiemce/HuntingService.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.fi.muni.pa165.creaturehunting.huntingexperiemce;
+
+import java.util.List;
+
+/**
+ *
+ * @author Fita
+ */
+public interface HuntingService {
+
+    /**
+     * Create a hunting experience.
+     * @param huntExpTrans instance of Hunting experince 
+     */
+    public void create(HuntingExperienceTrans huntExpTrans);
+    
+    /**
+     * Update the hunting experience.
+     * @param huntExpTrans instance of Hunting experince 
+     */
+    public void update(HuntingExperienceTrans huntExpTrans);
+    
+    /**
+     * Delete the hunting experience
+     * @param huntExpTrans instance of Hunting experince 
+     */
+    public void delete(HuntingExperienceTrans huntExpTrans);
+    
+    /**
+     * Find Hunting experience in database.
+     * @param huntExpTrans instance of Hunting experince 
+     * @return Hunting experience transefer object
+     */
+    public HuntingExperienceTrans findHuntExp(HuntingExperienceTrans huntExpTrans);
+    
+    /**
+     * Find all entities of Hunting experience in database.
+     * @param huntExpTrans instance of Hunting experince 
+     * @return list of  Hunting experience transefer objects
+     */
+    public List<HuntingExperienceTrans> findAllHuntExp(HuntingExperienceTrans huntExpTrans);     
+    
+    /**
+     * 
+     * @param creatureTrans instance of creatureTrans
+     * @param effeciency paramentr of effeciency of the weapon
+     * @return  list of weapon transefer object
+     */
+    public List<WeaponTrans> findEfficientWeapons(CreatureTrans creatureTrans, int effeciency);
+}
---
+++ CreatureHunting/creaturehunting-service/src/main/java/cz/fi/muni/pa165/creaturehunting/huntingexperiemce/HuntingServiceImpl.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.fi.muni.pa165.creaturehunting.huntingexperiemce;
+
+import java.util.List;
+
+
+public class HuntingServiceImpl implements HuntingService {
+
+    public void create(HuntingExperienceTrans huntExpTrans) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void update(HuntingExperienceTrans huntExpTrans) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void delete(HuntingExperienceTrans huntExpTrans) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public HuntingExperienceTrans findHuntExp(HuntingExperienceTrans huntExpTrans) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<HuntingExperienceTrans> findAllHuntExp(HuntingExperienceTrans huntExpTrans) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<WeaponTrans> findEfficientWeapons(CreatureTrans creatureTrans, int effeciency) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
