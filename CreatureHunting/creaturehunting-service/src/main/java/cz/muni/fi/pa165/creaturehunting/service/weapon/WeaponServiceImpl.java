7bafcd8b9a8f7866ce1e4613da35cd2084ce6ca1
Ladushka <kralickova.lebka@gmail.com>
2014-11-02 19:40:17 +0100

    Vytvoreny muj package bez implementace.

---
+++ CreatureHunting/creaturehunting-service/src/main/java/cz/muni/fi/pa165/creaturehunting/service/weapon/WeaponServiceImpl.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.creaturehunting.service.weapon;
+
+/**
+ *
+ * @author laduska
+ */
+public class WeaponServiceImpl {
+    
+}
