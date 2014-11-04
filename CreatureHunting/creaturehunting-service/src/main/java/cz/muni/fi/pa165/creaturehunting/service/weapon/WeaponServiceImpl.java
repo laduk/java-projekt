4936e1e654d1806edc231fa143dd56aa9c4aed99
Ladushka <kralickova.lebka@gmail.com>
2014-11-04 01:11:27 +0100

    ServiceImpl.

--- CreatureHunting/creaturehunting-service/src/main/java/cz/muni/fi/pa165/creaturehunting/service/weapon/WeaponServiceImpl.java
+++ CreatureHunting/creaturehunting-service/src/main/java/cz/muni/fi/pa165/creaturehunting/service/weapon/WeaponServiceImpl.java
@@ -4,6 +4,9 @@
  */
 package cz.muni.fi.pa165.creaturehunting.service.weapon;
 
+import cz.muni.fi.pa165.creaturehunting.dao.weapon.Weapon;
+import cz.muni.fi.pa165.creaturehunting.dao.weapon.WeaponDAO;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,24 +15,91 @@ import java.util.List;
  */
 public class WeaponServiceImpl implements WeaponService {
 
+    private WeaponDAO weaponDao;
+
+    public void setWeaponDao(WeaponDAO weaponDao) {
+        this.weaponDao = weaponDao;
+    }
+
+    /**
+     * Create a weapon.
+     *
+     * @param weaponDTO - data transfer object as a source of data
+     */
     public void create(WeaponDTO weaponDTO) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        if (weaponDTO == null) {
+            throw new NullPointerException("WeaponDTO argument for creation of entity cannot be null.");
+        }
+
+        //Weapon weapon = WeaponTranformation.transformToDTO(weaponDTO);
+        weaponDao.createWeapon(WeaponTranformation.transformToDTO(weaponDTO));
     }
 
+    /**
+     * Update a weapon.
+     *
+     * @param weaponDTO - data transfer object as a source of data
+     */
     public void update(WeaponDTO weaponDTO) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (weaponDTO == null) {
+            throw new NullPointerException("WeaponDTO argument for updating of entity cannot be null.");
+        }
+
+        //Weapon weapon = WeaponTranformation.transformToDTO(weaponDTO);
+        weaponDao.updateWeapon(WeaponTranformation.transformToDTO(weaponDTO));
     }
 
+    
+    
+    /**
+     * Delete a weapon.
+     *
+     * @param weaponDTO - data transfer object as a source of data
+     */
     public void delete(WeaponDTO weaponDTO) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (weaponDTO == null) {
+            throw new NullPointerException("WeaponDTO argument for deleting of entity cannot be null.");
+        }
+        //Weapon weapon = WeaponTranformation.transformToDTO(weaponDTO);
+        weaponDao.deleteWeapon(WeaponTranformation.transformToDTO(weaponDTO));
     }
 
+    
+    
+    
+    /**
+     * Find a weapon by ID.
+     *
+     * @param weaponDTO - data transfer object as a source of data
+     * @return weapon data transfer object
+     */
     public WeaponDTO findWeapon(long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        if (id < 0) {
+            throw new IllegalArgumentException("ID of the wanted weapon cannot be negative");
+        }
 
-    public List<WeaponDTO> findAllWeapons() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return WeaponTranformation.transformToEntity(weaponDao.findWeapon(id));
     }
     
+    
+    
+    
+    /**
+     * Find all weapons.
+     *
+     * @return list of weapon data transfer objects
+     */
+    public List<WeaponDTO> findAllWeapons() {
+        
+        List<Weapon> weapons = weaponDao.findAllWeapons();
+        
+        List<WeaponDTO> weaponsDto = new ArrayList<WeaponDTO>();
+        
+        for(Weapon wep : weapons){
+            weaponsDto.add(WeaponTransformation.transformToDto(wep));
+        }
+        
+        return weaponsDto;
+    }
 }
