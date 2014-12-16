4f0a350fb41ec2a5e89fe921a83d3ae920e7c4e4
'Ladushka' <kralickova.lebka@gmail.com>
2014-12-16 15:47:13 +0100

    Zprovozneny rest.

---
+++ CreatureHunting/creaturehunting-web/src/main/java/cz/muni/fi/pa165/creaturehunting/rest/ApplicationConfig.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.creaturehunting.rest;
+
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author laduska
+ */
+@javax.ws.rs.ApplicationPath("/rest")
+public class ApplicationConfig extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<Class<?>>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(cz.muni.fi.pa165.creaturehunting.rest.AreaRest.class);
+        resources.add(cz.muni.fi.pa165.creaturehunting.rest.CreatureRest.class);
+        resources.add(cz.muni.fi.pa165.creaturehunting.rest.WeaponRest.class);
+    }
+}
