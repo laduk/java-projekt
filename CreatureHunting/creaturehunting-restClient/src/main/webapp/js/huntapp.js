d93dbe5b422a12655d681fb3f9072a7ff118f0d8
Flaj <ondrej.flajsman@gmail.com>
2014-12-18 22:38:25 +0100

    Zavedene stavy ->Jmena kontroleru, nazvy html.

--- CreatureHunting/creaturehunting-restClient/src/main/webapp/js/huntapp.js
+++ CreatureHunting/creaturehunting-restClient/src/main/webapp/js/huntapp.js
@@ -7,7 +7,36 @@ angular.module('huntApp').config(function($stateProvider) {
         url: '/creatures',
         templateUrl: 'creatures.html',
         controller: 'CreatureListController'
+    }).state('newCreature', {
+        url: '/creatures/new',
+        templateUrl: 'creatures_add.html',
+        controller: 'CreatureAddController'
+    }).state('editCreature', {
+        url: '/creatures/edit',
+        templateUrl: 'creatures_edit.html',
+        controller: 'CreatureEditController'
+    }).state('deleteCreature', {
+        url: '/creatures/delete',
+        templateUrl: 'creatures_delete.html',
+        controller: 'CreatureDeleteController'
+    }).state('areas', {
+        url: '/areas',
+        templateUrl: 'areas.html',
+        controller: 'AreaListController'
+    }).state('newArea', {
+        url: '/areas/new',
+        templateUrl: 'areas_add.html',
+        controller: 'AreaAddController'
+    }).state('editArea', {
+        url: '/areas/edit',
+        templateUrl: 'areas_edit.html',
+        controller: 'AreaEditController'
+    }).state('deleteArea', {
+        url: '/areas/delete',
+        templateUrl: 'areas_delete.html',
+        controller: 'AreaDeleteController'
     });
+    
     
 }).run(function($state) {
   $state.go('index');
