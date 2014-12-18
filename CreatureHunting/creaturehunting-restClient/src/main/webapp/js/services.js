76256339b47fb31567369374412325e4e7b88adc
Flaj <ondrej.flajsman@gmail.com>
2014-12-18 21:34:37 +0100

    Services pridane odkazy.

--- CreatureHunting/creaturehunting-restClient/src/main/webapp/js/services.js
+++ CreatureHunting/creaturehunting-restClient/src/main/webapp/js/services.js
@@ -1,25 +1,52 @@
 angular.module('huntApp.services', [])
-.factory('Creature', function($resource){
-    return $resource('http://localhost\:8080/pa165/rest/creatures/:id',{id: '@id'}, {
-//          update: {
-//              method: 'PUT',
-//            interceptor: {responseError : showAlert}
-//          },
-          query: {
-              method: 'GET', isArray:true, 
-              interceptor: {responseError : showAlert}
-          }//,
-//          get: {
-//               method: 'GET', 
-//              interceptor: {responseError  : showAlert}         
-//          },
-//          save: {
-//              method: 'POST', 
-//              interceptor: {responseError : showAlert}  
-//          },
-//          remove: {
-//              method: 'DELETE',
-//            interceptor: {responseError : showAlert}  
-//          }
+.factory('Area', function($resource){
+    return $resource('http://localhost:8080/pa165/rest/areas/:id',{id: '@id'}, {
+        getA: {
+            method: 'GET', isArray:true, 
+            interceptor: {responseError : showAlert}
+        },
+        get: {
+            method: 'GET', 
+            interceptor: {responseError  : showAlert}         
+        },
+        save: {
+            method: 'POST', 
+            interceptor: {responseError : showAlert}  
+        },
+        update: {
+            method: 'PUT',
+            interceptor: {responseError : showAlert}
+        },       
+        remove: {
+              method: 'DELETE',
+            interceptor: {responseError : showAlert}  
+        }
     });
 })
+        
+.factory('Creature', function($resource){
+    return $resource('http://localhost:8080/pa165/rest/creatures/:id',{id: '@id'}, {
+        getA: {
+            method: 'GET', isArray:true, 
+            interceptor: {responseError : showAlert}
+        },
+        get: {
+            method: 'GET', 
+            interceptor: {responseError  : showAlert}         
+        },
+        save: {
+            method: 'POST', 
+            interceptor: {responseError : showAlert}  
+        },
+        update: {
+            method: 'PUT',
+            interceptor: {responseError : showAlert}
+        },       
+        remove: {
+            method: 'DELETE',
+            interceptor: {responseError : showAlert}  
+        }
+    });
+});
+
+function showAlert(){alert("Service is unavailable, try it again.");}
