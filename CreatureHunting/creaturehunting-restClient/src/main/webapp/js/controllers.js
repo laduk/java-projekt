f5f16958c416a2114ad328ccb449927261fd2e0e
radozaj <radozaj@centrum.sk>
2014-12-19 12:09:34 +0100

    area_add pociatky + jendodtne cislo u html (2. pokus)

--- CreatureHunting/creaturehunting-restClient/src/main/webapp/js/controllers.js
+++ CreatureHunting/creaturehunting-restClient/src/main/webapp/js/controllers.js
@@ -2,6 +2,14 @@ angular.module('huntApp.controllers', [])
 .controller('AreaListController',function($scope, $state, Area){
     $scope.areas = Area.getAll();
 })
+.controller('AreaAddController', function($scope, $state, Area) {
+    $scope.Area = new Area();
+    $scope.addArea = function(){
+        $scope.Area.$save(function() {
+            $state.go('areas');
+        });
+    };
+})
 .controller('CreatureListController',function($scope, $state, Creature){
     $scope.creatures = Creature.getAll();
 });
